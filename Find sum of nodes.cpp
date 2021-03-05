a93b9028ff220276c38617e0101321102c6b2b30
Diksha  Rajput <67539159+Diksha8448@users.noreply.github.com>
2021-03-05 21:31:48 +0530

    05/02/2021 DP

---
+++ Find sum of nodes.cpp
@@ -0,0 +1,45 @@
+/*
+Given a generic tree, find and return the sum of all nodes present in the given tree.
+
+Sample Input 1:
+10 3 20 30 40 2 40 50 0 0 0 0 
+Sample Output 1:
+190
+
+*/
+// Following is the given TreeNode structure.
+/*
+
+template <typename T>
+class TreeNode {
+public:
+    T data;
+    vector<TreeNode<T>*> children;
+    
+    TreeNode(T data) {
+        this->data = data;
+    }
+    
+    ~TreeNode() {
+        for (int i = 0; i < children.size(); i++) {
+            delete children[i];
+        }
+    }
+    
+};
+*/
+
+
+int sumOfNodes(TreeNode<int>* root) {
+    /* Don't write main().
+     * Don't read input, it is passed as function argument.
+     * Return output and don't print it.
+     * Taking input and printing output is handled automatically. 
+     */
+    int rootData = root->data;
+    int smallSum=0;
+    for(int i=0;i<root->children.size();i++){
+       smallSum += sumOfNodes(root->children[i]); 
+    }
+    return smallSum+rootData;
+}
